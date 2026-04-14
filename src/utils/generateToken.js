14555be9981a2e58237f086b77a9e8a34738fe68
Juan Manuel <144147130+juanodsk@users.noreply.github.com>
2026-04-14 11:31:56 -0500

    Change cookie SameSite attribute to 'None'

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
   });
   return token;
